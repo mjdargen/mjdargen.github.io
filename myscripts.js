eb423df1d4b961ce32cc3d1239bde6dd96909380
mjdargen <mjdargen@ncsu.edu>
2019-12-19 10:53:25 -0500

    Updated email link

--- myscripts.js
+++ myscripts.js
@@ -96,7 +96,7 @@ function githubIconClick() {
 
 /* double clicking email icon */
 function emailIconClick() {
-	window.open('mailto:mjdargen@gmail.com');
+	location.href = "mailto:mjdargen@gmail.com";
 }
 
 /* double clicking ascii icon */
